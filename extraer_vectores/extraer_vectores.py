c6a1acfc8cad03d9928cf982a07c0e3d62aaa12a
Mar <mar.puigmarti@estudiantat.upc.edu>
2025-04-17 16:24:02 +0200

    Dockerfile de extraer_vectores

--- extraer_vectores/extraer_vectores.py
+++ extraer_vectores/extraer_vectores.py
@@ -60,7 +60,7 @@ def process_images():
 
 # Ejecutamos el servidor Flask
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
 
     #curl -X POST http://127.0.0.1:5000/process   -F "images=@../../MESSI.jpg"
     #python provarDetection.py /path/MESSI2.jpg
